864e6095bab2d1ff8be619240e387d9d5d0e1b6e
Adarsha Ghimire <adarshghimire2005@gmail.com>
2025-01-08 10:05:12 +0545

    Added script for Login page to validate email

---
+++ public/login.js
@@ -0,0 +1,20 @@
+document.querySelector(".login-form").addEventListener("submit", function (event)
+{
+    event.preventDefault();
+    document.querySelector(".login-message").textContent = "";
+    
+    const email = document.querySelector(".login-input").value;
+    if (!(validateEmail(email)))
+    {
+        document.querySelector(".login-message").textContent = "Please enter a valid KU email."
+    }
+    else
+    {
+        event.target.submit();
+    }
+})
+
+function validateEmail(email)
+{
+    return (/^[a-z]+[0-9]+@student.ku.edu.np$/.test(email));
+}
